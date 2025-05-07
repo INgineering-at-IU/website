f19063df2152d465d082579f892e972ff511e349
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2025-05-07 04:19:05 -0400

    Revert deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/website/',
-})
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/website/' : '/',
+    plugins: [react(), tailwindcss()], 
+  };
+});
