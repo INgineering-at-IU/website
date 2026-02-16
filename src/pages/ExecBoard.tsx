833aafee936ad94683d6dc1a104d182e0b681d6d
cookin99 <alexd7johnson@gmail.com>
2026-02-15 20:41:34 -0500

    Fix exec board image filenames - remove spaces

--- src/pages/ExecBoard.tsx
+++ src/pages/ExecBoard.tsx
@@ -10,26 +10,26 @@ function ExecPage() {
         {
             name: "Alex",
             position: "Director of Technology",
-            href: "/VP Tech new.jpeg",
+            href: "/VP-Tech-new.jpeg",
             color: "bg-white"
         },
             {
             name: "Benian",
             position: "Director of Marketing",
-            href: "/VP Marketing new.jpeg",
+            href: "/VP-Marketing-new.jpeg",
             color: "bg-white"        
         },
 
         {
             name: "Shashank",
             position: "Director of Operations",
-            href: "/VP OPS new.jpeg",
+            href: "/VP-OPS-new.jpeg",
             color: "bg-zinc-100"
         },
         {
             name: "Rabia",
             position: "President",
-            href: "/President new.jpeg",
+            href: "/President-new.jpeg",
             color: "bg-white"        
         },
     ]
